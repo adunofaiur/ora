82621752eaeda2fc153d65c004fcd52f6fb188e7
adunofaiur <matthew21573@gmail.com>
2014-10-24 11:53:26 -0500

    set up initial framework

---
+++ database/database.js
@@ -0,0 +1,12 @@
+/**
+ * We will use the following convention for our js files:
+ * 
+ * We make an object per file and define ALL functions as members of that object
+ * This iis to avoid collisions in our namespaces
+ */
+
+var Database = {};
+Database.someFunction = function(someArg, anotherArg){
+	var someValue = 0;
+	return someValue;
+}
