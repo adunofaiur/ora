fe1ba731cd71abc28986793dd1404316910dc532
unknown <anifan@L111C18.cs.tamu.edu>
2014-11-02 13:39:01 -0600

    updated framwork - this is real guys, this is real!

---
+++ graph/musicGraph.js
@@ -0,0 +1,11 @@
+/**
+ * We will use the following convention for our js files:
+ * 
+ * We make an object per file and define ALL functions as members of that object
+ * This iis to avoid collisions in our namespaces
+ */
+//A global variable. Be careful not to overwrite other global variables!
+var Graph = {};
+MusicGraph.initialize = function(){
+	
+}
